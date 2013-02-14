04661ca9cb24a3b256ae0d01f3c0bf5ccef93a1f
Stuart P. Bentley <stuart@testtrack4.com>
2013-02-14 08:39:02 -0500

    Working (but not fully tested) CLI

--- cli.js
+++ cli.js
@@ -1,4 +1,13 @@
 var argv = require("optimist").argv;
 var lhc = require("./lib/lhc.js");
 
-console.log(lhc.collide({target:"23145",limit:10}));
+var algorithm = argv.algorithm || 'sha1';
+var target = argv.target || argv._[0];
+//Not honestly sure it's a great idea to parameterize this, but...
+var limit = argv.limit;
+
+console.log(lhc.collide({
+  algo: algorithm,
+  target: target,
+  limit: limit
+}));
